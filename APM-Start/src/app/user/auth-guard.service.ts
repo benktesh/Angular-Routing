7a82aeee7a2321b972d5fc5bf63f0bcfaa47f91d
benktesh <benktesh@yahoo.com>
2018-07-19 16:51:45 -0500

    shares data with guards

--- APM-Start/src/app/user/auth-guard.service.ts
+++ APM-Start/src/app/user/auth-guard.service.ts
@@ -8,15 +8,14 @@ export class AuthGuard implements CanActivate {
 
     constructor(private authService: AuthService, private router: Router) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        return this.checkLoggedIn();
-
+        return this.checkLoggedIn(state.url);
     }
 
-    checkLoggedIn(): boolean {
+    checkLoggedIn(url:string): boolean {
         if (this.authService.isLoggedIn()) {
             return true;
         }
-
+        this.authService.redirectUrl = url;
         this.router.navigate(['/login']);
         return false;
     }

f523b9e234d4057514cc80f264d494326277ff41
benktesh <benktesh@yahoo.com>
2018-07-20 11:56:19 -0500

    Completes custom preloading strategy and finishes Lazy Loading

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth-guard.service';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 const ROUTES = [
     { path: 'welcome', component: WelcomeComponent },
-    { path: 'products', canActivate: [AuthGuard], loadChildren: 'app/products/product.module#ProductModule'},
+    { path: 'products', canActivate: [AuthGuard], data : {preload:true}, loadChildren: 'app/products/product.module#ProductModule' },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
-    
+
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(ROUTES, {enableTracing:true, preloadingStrategy: PreloadAllModules} )
+        RouterModule.forRoot(ROUTES, { enableTracing: true, preloadingStrategy: SelectiveStrategy })
     ],
+    providers: [SelectiveStrategy],
     exports: [RouterModule]
 
 })
